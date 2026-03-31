ada888c5eb4b6fbdb565e44fd15c28e0feb50127
BrunoT4 <91289803+BrunoT4@users.noreply.github.com>
2026-03-31 15:38:20 -0500

    Contraction

--- bench.js
+++ bench.js
@@ -6,14 +6,7 @@ const hello = fs.readFileSync("bf/hello.bf");
 const bench = fs.readFileSync("bf/bench.bf");
 const hanoi = fs.readFileSync("bf/hanoi.bf");
 const mande = fs.readFileSync("bf/mandelbrot.bf");
-
-import { run as interp1 } from "./step-1/interp.js";
-import { run as interp2 } from "./step-2/interp.js";
-import { run as interp3 } from "./step-3/interp.js";
-import { run as interp4 } from "./step-4/interp.js";
-import { run as interp5 } from "./step-5/interp.js";
-import { run as interp6 } from "./step-6/interp.js";
-import { run as interp7 } from "./step-7/interp.js";
+import { run as interp } from "./interp.js";
 
 let benchmark = function (method, iterations, bytes) {
   let time = 0;
@@ -42,21 +35,12 @@ let benchmark = function (method, iterations, bytes) {
 };
 
 let marks = {};
-marks["1: hello"] = benchmark(interp1, 100, hello);
-marks["2: hello"] = benchmark(interp2, 100, hello);
-marks["3: hello"] = benchmark(interp3, 100, hello);
-marks["4: hello"] = benchmark(interp4, 100, hello);
-marks["5: hello"] = benchmark(interp5, 100, hello);
-marks["6: hello"] = benchmark(interp6, 100, hello);
-marks["7: hello"] = benchmark(interp7, 100, hello);
+marks["1: hello"] = benchmark(interp, 100, hello);
 
-marks["1: bench"] = benchmark(interp1, 5, bench);
-marks["7: bench"] = benchmark(interp7, 5, bench);
+marks["2: bench"] = benchmark(interp, 5, bench);
 
-marks["1: hanoi"] = benchmark(interp1, 1, hanoi);
-marks["7: hanoi"] = benchmark(interp7, 1, hanoi);
+marks["3: hanoi"] = benchmark(interp, 1, hanoi);
 
-marks["1: mandel"] = benchmark(interp1, 1, mande);
-marks["7: mandel"] = benchmark(interp7, 1, mande);
+marks["4: mandel"] = benchmark(interp, 1, mande);
 
 console.log(marks);
